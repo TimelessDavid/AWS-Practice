348319c0e22497b167334cea3f71d3ddbc2a526c
TimelessDavid <140206541+TimelessDavid@users.noreply.github.com>
2025-10-01 02:20:25 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_aeiscu0am', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'rb5qvlbgaouamnp9kvej8me1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_QlBKeQdYs', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '16985gd8mm72n751fel94e1li9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
